c7288d1063cdedcb9452820b2932a8d808fb4594
Timo Furrer <tuxtimo@gmail.com>
2017-02-26 10:33:26 +0100

    release: 1.1.0
    Signed-off-by: Timo Furrer <tuxtimo@gmail.com>

--- tagexpressions/__init__.py
+++ tagexpressions/__init__.py
@@ -6,7 +6,7 @@ Package to parse logical tag expressions
 
 from .parser import parse, evaluate
 
-__VERSION__ = '1.0.0'
+__VERSION__ = '1.1.0'
 
 # only expose the parse function
 __all__ = ['parse', 'evaluate']
