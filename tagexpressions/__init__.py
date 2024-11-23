a55625523dbf0046fe3e03f5ac612d6d699a8490
fliiiix <hi@l33t.name>
2024-11-23 16:44:20 +0100

    release: v2.0.1

--- tagexpressions/__init__.py
+++ tagexpressions/__init__.py
@@ -6,7 +6,7 @@ Package to parse logical tag expressions
 
 from .parser import parse, evaluate
 
-__VERSION__ = '2.0.0'
+__VERSION__ = '2.0.1'
 
 # only expose the parse function
 __all__ = ['parse', 'evaluate']
